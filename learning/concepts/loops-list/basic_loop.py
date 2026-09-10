34b9a2341f9fa8a1f7fc4712a79f9759786ce229
Bibash Thapa <bibashthapa1432@gmail.com>
2026-09-10 17:53:44 +0545

    feat: add basic loop demonstrations in basic_loop.py

---
+++ learning/concepts/loops-list/basic_loop.py
@@ -0,0 +1,21 @@
+
+for f in range(1, 11, 2):   # (Start, stop, step)
+    print(f)
+
+for f in range (1,5):
+    print(f)
+
+print("================")
+count = 0
+while count < 5:
+    print(count)
+    count +=1
+
+print("=================")
+for i in range(1,10):
+    if i ==5:
+        break
+    elif i % 2 ==0:
+        print(i)
+        continue
+    
