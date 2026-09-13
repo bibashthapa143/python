afd4be79d0c2802a14151833bff9aaad5c998c0b
Bibash Thapa <bibashthapa1432@gmail.com>
2026-09-13 23:59:03 +0545

    fix: set default scale in grade calculator and improve score checks

--- learning/concepts/modules/grade_calculator.py
+++ learning/concepts/modules/grade_calculator.py
@@ -2,16 +2,25 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--score", type=int)
-parser.add_argument("--scale", choices=["normal", "strict"])
+parser.add_argument("--scale", choices=["normal", "strict"], default="normal")
 
 args = parser.parse_args()
-if args.score > 90:
-    print("A")
-elif args.score>80:
-    print("B")
-elif args.score > 70:
-    print("C")
+
+if args.scale == "normal":
+    if args.score >= 90:
+        print("A")
+    elif args.score >= 80:
+        print("B")
+    elif args.score >= 70:
+        print("C")
+    else:
+        print("F")
 else:
-    print("F")
-
-
+    if args.score >= 95:
+        print("A")
+    elif args.score >= 85:
+        print("B")
+    elif args.score >= 75:
+        print("C")
+    else:
+        print("F")
