ac6e208a6eb688c9394fce6cb3e7f8453370e684
Bibash Thapa <bibashthapa1432@gmail.com>
2026-09-22 23:36:05 +0545

    style: improve code formatting and add comments for clarity in praactice.py

--- learning/concepts/file-io/praactice.py
+++ learning/concepts/file-io/praactice.py
@@ -1,26 +1,35 @@
-with open ("ports.txt", "r") as f:
-    ports=[int(line.strip()) for line in f]
+# Read ports as integers
+with open("ports.txt", "r") as f:
+    ports = [int(line.strip()) for line in f]
+
 print(ports)
 
 print("-------------------------------")
+
+# Read and print the file content
 with open("ports.txt", "r") as file:
     content = file.read()
     print(content)
 
 print("------------------------------")
-with open("ports.txt","a+") as file:
+
+# Append new ports and read the file
+with open("ports.txt", "a+") as file:
     file.write("500\n")
     file.write("100\n")
     file.seek(0)
-    content= file.read()
+    content = file.read()
     print(content)
 
 print("----------------------")
-with open("ports.txt","w+") as file:
-    a="Apple\n"
-    b="ball"
+
+# Overwrite the file and read the new content
+with open("ports.txt", "w+") as file:
+    a = "Apple\n"
+    b = "ball"
+
     file.write(a)
     file.write(b)
     file.seek(0)
-    print(file.read())
 
+    print(file.read())
