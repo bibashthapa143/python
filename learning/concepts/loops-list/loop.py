3c4dd2e2a2b5d887bc11c84b6c07150f6a803f94
Bibash Thapa <bibashthapa1432@gmail.com>
2026-09-22 23:34:32 +0545

    style: improve comments for clarity in loop.py

--- learning/concepts/loops-list/loop.py
+++ learning/concepts/loops-list/loop.py
@@ -1,13 +1,16 @@
+# Print numbers from 0 to 4
 for i in range(5):
     print(i)
 
 print("--------------------------")
 
-for i in range(1,7):
+# Print numbers from 1 to 6
+for i in range(1, 7):
     print(i)
 
 print("--------------------------")
 
+# Print numbers from 1 to 5 using a while loop
 n = 1
 
 while n <= 5:
