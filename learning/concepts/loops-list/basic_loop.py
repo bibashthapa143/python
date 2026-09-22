ea35e81c76d6231402a8495827d38e9d0dae61ae
Bibash Thapa <bibashthapa1432@gmail.com>
2026-09-22 23:56:15 +0545

    style: improve comments for clarity and fix formatting in basic_loop.py

--- learning/concepts/loops-list/basic_loop.py
+++ learning/concepts/loops-list/basic_loop.py
@@ -1,21 +1,25 @@
-
-for f in range(1, 11, 2):   # (Start, stop, step)
+# Print odd numbers from 1 to 9
+for f in range(1, 11, 2):
     print(f)
 
-for f in range (1,5):
+# Print numbers from 1 to 4
+for f in range(1, 5):
     print(f)
 
 print("================")
+
+# Print numbers from 0 to 4
 count = 0
 while count < 5:
     print(count)
-    count +=1
+    count += 1
 
 print("=================")
-for i in range(1,10):
-    if i ==5:
+
+# Print even numbers until 5 is reached
+for i in range(1, 10):
+    if i == 5:
         break
-    elif i % 2 ==0:
+    elif i % 2 == 0:
         print(i)
         continue
-    
